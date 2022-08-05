6288fbe910230d9262958b67503c24e350869e0f
yungit4455 <yjhama1532@gmail.com>
2022-08-05 11:43:40 +0900

    middleware 작동 방식  확인

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+// app.use로 전달하는 이 함수는 모든 요청에 대해 실행된다.
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!!');
+});
+
+// const server = http.createServer(app);
+// server.listen(3000);
+app.listen(3000); // 위의 코드와 동등한 결과를 간단하게 표현
