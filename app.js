57606d211ceead84cb13b9e5a17e5b35a35e76c6
yungit4455 <yjhama1532@gmail.com>
2022-08-07 16:31:39 +0900

    EJS를 이용한 템플릿 작성 완료

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ const path = require('path');
 // third party modules
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({ 
-    layoutsDir: 'views/layouts/', 
-    defaultLayout: 'main-layout', 
-    extname: 'hbs' 
-}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', expressHbs({ 
+//     layoutsDir: 'views/layouts/', 
+//     defaultLayout: 'main-layout', 
+//     extname: 'hbs' 
+// }));
+// app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 // 내가 임포트한 파일
@@ -31,7 +32,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found!' });
+    res.status(404).render('404', { 
+        pageTitle: 'Page Not Found!', 
+        path: ''
+    });
 });
 
 // const server = http.createServer(app);
