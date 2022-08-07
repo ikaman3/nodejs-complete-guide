2d67909b9c167ef6b2969ee42b9399c60df74b60
yungit4455 <yjhama1532@gmail.com>
2022-08-07 16:37:31 +0900

    사용하지 않을 html, pug, hbs 파일 삭제, 주석 처리된 코드 삭제

--- app.js
+++ app.js
@@ -7,17 +7,9 @@ const path = require('path');
 // third party modules
 const express = require('express');
 const bodyParser = require('body-parser');
-// const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', expressHbs({ 
-//     layoutsDir: 'views/layouts/', 
-//     defaultLayout: 'main-layout', 
-//     extname: 'hbs' 
-// }));
-// app.set('view engine', 'hbs');
-// app.set('view engine', 'pug');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
