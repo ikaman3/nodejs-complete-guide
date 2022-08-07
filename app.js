478a6374355eb589920fef705d9cb4f87b56e546
yungit4455 <yjhama1532@gmail.com>
2022-08-07 18:43:38 +0900

    error controller 생성

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -23,12 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { 
-        pageTitle: 'Page Not Found!', 
-        path: ''
-    });
-});
+app.use(errorController.get404);
 
 // const server = http.createServer(app);
 // server.listen(3000);
