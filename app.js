a010a302f101f42c817ccc6c4149260c82d94d06
yungit4455 <yjhama1532@gmail.com>
2022-08-12 16:03:09 +0900

    일대다, 다대다 관계

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
-
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -42,6 +43,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constranints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
 .sync()
