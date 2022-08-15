235a7563e1a3204d2dc330b9f2d44aef945639a8
yungit4455 <yjhama1532@gmail.com>
2022-08-15 18:50:31 +0900

    mongoDB 연결, 모든 제품 가져오기, 단일 제품 가져오기 미완성, 에러 발생

--- app.js
+++ app.js
@@ -9,14 +9,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
-
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -31,12 +24,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1)
-    .then(user => {
-        req.user = user;
-        next();
-    })
-    .catch(err => console.log(err));
+//     User.findByPk(1)
+//     .then(user => {
+//         req.user = user;
+//         next();
+//     })
+//     .catch(err => console.log(err));
+    next();
 });
 
 app.use('/admin', adminRoutes);
@@ -44,33 +38,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constranints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-
-sequelize
-.sync()
-.then(result => {
-    return User.findByPk(1);
-})
-.then(user => {
-    if (!user) {
-        return User.create({ name: 'Max', email: 'test@test.com' });
-    }
-    return user;
-})
-.then(user => {
-    return user.createCart();
-})
-.then(cart => {
+mongoConnect(() => {
     app.listen(3000);
-})
-.catch(err => {
-    console.log(err);
 });
