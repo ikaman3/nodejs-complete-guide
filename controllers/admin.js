5f86c35bc2ad6322df81128bda16b0c153677008
yungit4455 <yjhama1532@gmail.com>
2022-08-17 18:22:24 +0900

    제품 편집, 디테일, 업데이트 기능 부활

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 
+
 exports.getAddProduct = (req, res, next) => {    
     res.render('admin/edit-product', { 
         pageTitle: 'Add Product', 
@@ -63,17 +64,28 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    Product.findById(prodId)
-      .then(product => {
-        product.title = updatedTitle;
-        product.price = updatedPrice;
-        product.description = updatedDesc;
-        product.imageUrl = updatedImageUrl;
-        return product.save();
-      })
-      .then(result => {
+    const product = new Product(
+        updatedTitle, 
+        updatedPrice,
+        updatedDesc, 
+        updatedImageUrl, 
+        prodId
+    );
+    product
+    .save()
+    .then(result => {
         console.log('UPDATED PRODUCT!');
         res.redirect('/admin/products');
       })
       .catch(err => console.log(err));
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.deleteById(prodId)
+    .then(() => {
+        console.log('DESTROYED PRODUCT');
+        res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
+}
