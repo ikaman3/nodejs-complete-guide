15ab161e9f858a63679a7d4bc13c2f60897c1db0
yungit4455 <yjhama1532@gmail.com>
2022-08-31 16:49:00 +0900

    에러 해결: 라우트에서 로그인 검증 논리를 추가하지 않았다.

--- routes/auth.js
+++ routes/auth.js
@@ -10,40 +10,46 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+    '/login', 
+    [
+        body('email')
+          .isEmail()
+          .withMessage('Please enter a valid email address.'),
+        body('password', 'Password has to be valid.')
+          .isLength({ min: 5 })
+          .isAlphanumeric()
+    ],   
+    authController.postLogin
+);
 
 router.post(
     '/signup',
     [
-        check('email')
+      check('email')
         .isEmail()
         .withMessage('Please enter a valid email.')
         .custom((value, {req}) => {
-            // if (value === 'test@test.com') {
-            //     throw new Error('This email address if forbidden.');
-            // }
-            // return true;
-
-            return User.findOne({ email: value }).then(userDoc => {
-                if (userDoc) {
-                    return Promise.reject(
-                        'E-mail exists already, please pick a different one.'
-                    );
-                }
-            })
-        }),
-        body(
-            'password', 
-            'Please enter a password with only numbers and text and at lease 5 characters.'
-        )
-            .isLength({ min: 5 })
-            .isAlphanumeric(),
-        body('confirmPassword').custom((value, { req }) => {
-            if (value !== req.body.password) {
-                throw new Error('Passwords have to match!');
+          return User.findOne({ email: value }).then(userDoc => {
+            if (userDoc) {
+              return Promise.reject(
+                'E-mail exists already, please pick a different one.'
+              );
             }
-            return true;
-        })
+          })
+        }),
+      body(
+        'password', 
+        'Please enter a password with only numbers and text and at lease 5 characters.'
+      )
+        .isLength({ min: 5 })
+        .isAlphanumeric(),
+      body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
+      })
     ],
     authController.postSignup
 );
