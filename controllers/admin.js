478c7b249fbf6109666df0366ed6834367329fdd
yungit4455 <yjhama1532@gmail.com>
2022-09-02 14:01:57 +0900

    컨트롤러 전체에 에러 처리 구문 업데이트

--- controllers/admin.js
+++ controllers/admin.js
@@ -40,7 +40,7 @@ exports.postAddProduct = (req, res, next) => {
   }
 
   const product = new Product({
-    _id: new mongoose.Types.ObjectId('630f18086a916046de70a573'),
+    // _id: new mongoose.Types.ObjectId('630f18086a916046de70a573'),
     title: title,
     price: price,
     description: description,
@@ -84,7 +84,7 @@ exports.getEditProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
-      throw new Error('Dummy');
+      // throw new Error('Dummy');
       if (!product) {
         return res.redirect('/');
       }
@@ -145,7 +145,11 @@ exports.postEditProduct = (req, res, next) => {
           res.redirect('/admin/products');
         });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
@@ -160,7 +164,11 @@ exports.getProducts = (req, res, next) => {
         path: '/admin/products'
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
@@ -170,5 +178,9 @@ exports.postDeleteProduct = (req, res, next) => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 }; 
--- controllers/auth.js
+++ controllers/auth.js
@@ -117,7 +117,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -159,7 +163,9 @@ exports.postSignup = (req, res, next) => {
       });
     })
     .catch(err => {
-      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
     });
 };
 
@@ -213,7 +219,11 @@ exports.postReset = (req, res, next) => {
         `
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
   });
 };
 
@@ -239,7 +249,11 @@ exports.getNewPassword = (req, res, next) => {
       passwordToken: token
     });
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  });
 };
 
 exports.postNewPassword = (req, res, next) => {
@@ -266,5 +280,9 @@ exports.postNewPassword = (req, res, next) => {
   .then(result => {
     res.redirect('/login');
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
+  });
 };
