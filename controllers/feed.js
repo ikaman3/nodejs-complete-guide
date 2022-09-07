7a985a923225bad84a4c9c81d90d84603fd1e5e4
yungit4455 <yjhama1532@gmail.com>
2022-09-07 15:31:17 +0900

    단일 게시물 가져오기

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,16 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            { 
-                _id: '1',
-                title: 'First Post', 
-                content: 'This is the first post!', 
-                imageUrl: 'images/boat.jpg',
-                creator: {
-                    name: 'Max'
-                },
-                createdAt: new Date()
+    Post.find()
+        .then(posts => {
+            res.status(200).json({ message: 'Fetched posts successfully.', posts: posts });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
             }
-        ]
-    });
+            next(err);
+        });
 };
 
 exports.createPost = (req, res, next) => {
@@ -49,4 +45,24 @@ exports.createPost = (req, res, next) => {
             }
             next(err);
         });
+    };
+    
+    exports.getPost = (req, res, next) => {
+        const postId = req.params.postId;
+        Post
+            .findById(postId)
+            .then(post => {
+                if (!post) {
+                    const error = new Error('Could not find post.');
+                    error.statusCode = 404;
+                    throw error;
+                }
+                res.status(200).json({ message: 'Post fetched.', post: post });
+            })
+            .catch(err => {
+                if (!err.statusCode) {
+                    err.statusCode = 500;
+                }
+                next(err);
+            });
 };
