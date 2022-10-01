76c223e91e5a34a1792f5f4419864be6b97e7952
yungit4455 <yjhama1532@gmail.com>
2022-10-01 13:35:33 +0900

    is-auth middleware 테스트

--- test/start.js
+++ test/start.js
@@ -1,13 +1,16 @@
-const expect = require('chai').expect;
 
-it('should add numbers correctly', function() {
-    const num1 = 2;
-    const num2 = 3;
-     expect(num1 + num2).to.equal(5);
-})
 
-it('should not give a result of 6', function() {
-    const num1 = 3;
-    const num2 = 3;
-     expect(num1 + num2).not.to.equal(6);
-})
+// 테스트 자동화의 테스트를 위한 더미 코드
+// const expect = require('chai').expect;
+
+// it('should add numbers correctly', function() {
+//     const num1 = 2;
+//     const num2 = 3;
+//      expect(num1 + num2).to.equal(5);
+// })
+
+// it('should not give a result of 6', function() {
+//     const num1 = 3;
+//     const num2 = 3;
+//      expect(num1 + num2).not.to.equal(6);
+// })
