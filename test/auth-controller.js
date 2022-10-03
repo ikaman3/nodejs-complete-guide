287daf227143fd57770ff5a94b4cf9270ab74def
yungit4455 <yjhama1532@gmail.com>
2022-10-03 17:26:19 +0900

    테스트 DB 설정

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
@@ -23,5 +24,22 @@ describe('Auth Controller - Login', function() {
         });
 
         User.findOne.restore();
+    });
+
+    it('should send a response with a valid user status for an existing user', function(done) {
+        mongoose.connect('mongodb+srv://Max:sadqqwdsa@cluster0.bcol3h1.mongodb.net/test-messages')
+            .then(result => {
+                const user = new User({
+                    email: 'test@test.com',
+                    password: 'abc123',
+                    name: 'Tester',
+                    posts: []
+                });
+                return user.save();
+            })
+            .then(() => {
+                
+            })
+            .catch(err => console.log(err));
     });
 });
