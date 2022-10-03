bb3af22447eb568ab55448c23168236ff3fb7966
yungit4455 <yjhama1532@gmail.com>
2022-10-03 17:54:55 +0900

    DB 테스트에서 ID 중복 문제 해결

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -56,7 +56,13 @@ describe('Auth Controller - Login', function() {
                 AuthController.getUserStatus(req, res, () => {}).then(() => {
                     expect(res.statusCode).to.be.equal(200);
                     expect(res.userStatus).to.be.equal('I am new!');
-                    done();
+                    User.deleteMany({})
+                        .then(() => {
+                            return mongoose.disconnect();
+                        })
+                        .then(() => {
+                            done();
+                        });
                 });
             })
             .catch(err => console.log(err));
